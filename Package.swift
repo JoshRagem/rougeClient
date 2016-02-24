07cb67719b971660736d7baa2d2ab2a2185831ac
Joshua Barney <barney@whoop.com>
2016-02-23 23:24:00 -0500

    add in libuv

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     dependencies: [
-      .Package(url: "../CNCurses", versions: Version(0,0,1)..<Version(1,0,0))
+      .Package(url: "../CNCurses", versions: Version(0,0,1)..<Version(1,0,0)),
+      .Package(url: "../CLibUV", versions: Version(0,0,1)..<Version(1,0,0)) 
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,9 @@
 import CNCurses
+import CLibUV
+
+let loop = uv_default_loop()
+uv_run(loop, UV_RUN_DEFAULT)
+print("Event Loop: \(loop)")
 
 initscr()
 noecho()
